edcb0c425608a3ec44f4a21375fbf0bba2aff245
Andreas Stratakis <57875212+astratakis@users.noreply.github.com>
2025-04-22 16:25:22 +0300

    Updated main
    Now the input is always a qubo matrix

--- main.cpp
+++ main.cpp
@@ -1,31 +1,88 @@
 #include <iostream>
 #include <stdlib.h>
+#include <cstring>
 #include "annealing.h"
 
 using namespace std;
+
+void print_ising(double *h, double *J, unsigned int n)
+{
+    cout << n << endl;
+    for (unsigned int i = 0; i < n; i++)
+    {
+        cout << h[i] << (i < n - 1 ? " " : "");
+    }
+    cout << endl;
+    for (unsigned int i = 0; i < n; i++)
+    {
+        for (unsigned int j = 0; j < n; j++)
+        {
+            cout << J[i * n + j] << (j < n - 1 ? " " : "");
+        }
+        cout << endl;
+    }
+}
 
 int main()
 {
     unsigned int n;
     cin >> n;
 
-    double *h = (double *)malloc(n * sizeof(double));
-    double *J = (double *)malloc(n * n * sizeof(double));
-
-    for (unsigned int i = 0; i < n; i++)
-    {
-        cin >> h[i];
-    }
+    double *Q = (double *)malloc(n * n * sizeof(double));
 
     for (unsigned int i = 0; i < n; i++)
     {
         for (unsigned int j = 0; j < n; j++)
         {
-            cin >> J[i * n + j];
+            cin >> Q[i * n + j];
         }
     }
 
-    problem_t problem = initialize_problem(h, J, n, 100.0, 1.0, 1.0, 1000);
-    evolve(&problem, 2);
+    double *J = (double *)malloc(n * n * sizeof(double));
+    double *h = (double *)malloc(n * sizeof(double));
+
+    memset(J, 0, n * n * sizeof(double));
+    memset(h, 0, n * sizeof(double));
+
+    for (unsigned int i = 0; i < n - 1; i++)
+    {
+        for (unsigned int j = i + 1; j < n; j++)
+        {
+            J[i * n + j] = Q[i * n + j] / 4;
+        }
+    }
+
+    for (unsigned int i = 0; i < n; i++)
+    {
+        h[i] = Q[i * n + i] / 2;
+
+        for (unsigned int j = i + 1; j < n; j++)
+        {
+            h[i] += Q[i * n + j] / 4;
+        }
+    }
+
+    double C = 0.0;
+
+    for (unsigned int i = 0; i < n - 1; i++)
+    {
+        for (unsigned int j = i + 1; j < n; j++)
+        {
+            C += Q[i * n + j] / 4;
+        }
+    }
+
+    for (unsigned int i = 0; i < n; i++)
+    {
+        C += Q[i * n + i] / 2;
+    }
+
+    free(Q);
+
+    print_ising(h, J, n);
+
+    free(h);
+    free(J);
+
     return 0;
 }
